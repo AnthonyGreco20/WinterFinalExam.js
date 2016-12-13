76345de0895e29cf8e40ca449cf14a32ba71bc6d
AnthonyGreco20 <9agreco@stpatrick.org>
2016-12-13 13:54:54 -0600

    more coding

---
+++ WinterFinalExam.js-master/First-Semester-Final-master/GrecoFinalPassword.js
@@ -0,0 +1,18 @@
+class Password{
+  //The first part of the class.
+  constructor(publickey,privatekey){
+    this.publickey="characters";
+    this.privatekey="xxxx-xxxx-xxxx";
+  }
+  //Instance functions below this comment.
+  validPublicKey(){
+    for()
+  }
+  validPrivateKey(){
+    for()
+  }
+  //Static function below this comment.
+  makePrivateKey(){
+
+  }
+}
