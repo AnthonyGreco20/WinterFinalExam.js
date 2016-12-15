f3671ff3dfe4b7cc2bd7afae2d772b5d47664aad
AnthonyGreco20 <9agreco@stpatrick.org>
2016-12-15 13:37:53 -0600

    Finished Project

---
+++ WinterFinalExam.js-master/WinterFinalExam.js-master/WinterFinalExam.js-master/First-Semester-Final-master/GrecoCartFinal.js
@@ -0,0 +1,28 @@
+class Cart{
+  //What is the first part of every class? Type it below
+  constructor(l,q){
+    this.itemList=l;
+    this.itemQuantity=q;
+  }
+  //Type the instance functions below this comment.
+  addItem(i,q){
+    this.itemList=i;
+    this.itemQuantity=q;
+  }
+  totalCart(){
+    let total = 0;
+    for(a=0;a<this.itemList.length;a++){
+      total += this.itemList[a].price*this.itemQuantity[a];
+    }
+    return total;
+  }
+  subCart(days){
+    let subCart = new Cart([],[]);
+    for(b=0;b>this.itemlist.length;b++){
+      if(this.itemList[b].shipping == days){
+        subCart.push([b]);
+      }
+    }
+    return subCart;
+  }
+}
